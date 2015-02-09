ae3823fe1eb1a427b0b6800d586a41aff561c279
Mathias Jernberg <mathias.jernberg@valtech.se>
2015-02-09 14:38:03 +0100

    added test

---
+++ consolez/TestClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace consolez
+{
+    class TestClass
+    {
+        [Test]
+        public void does_one_plus_one_equal_two()
+        {
+            Assert.AreEqual(1+1,2);
+        }
+    }
+}
