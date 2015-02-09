bcd6d16c06a242fcc567cb745d1bdff9a58f07ca
Mathias Jernberg <mathias.jernberg@valtech.se>
2015-02-09 14:14:51 +0100

    gfjh

--- consolez/Program.cs
+++ consolez/Program.cs
@@ -10,7 +10,7 @@ namespace consolez
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World2!");
             Console.ReadKey();
         }
     }
