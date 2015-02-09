1d26adf8aeec36707aa9008b900b11ca5499ec91
Mathias Jernberg <mathias.jernberg@valtech.se>
2015-02-09 15:08:02 +0100

    tests

---
+++ Test/TestClass.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace Test
+{
+    [TestFixture]
+    class TestClass
+    {
+        [Test]
+        public void does_one_plus_one_equal_two()
+        {
+            Assert.AreEqual(1 + 1, 2);
+        }
+    }
+}
